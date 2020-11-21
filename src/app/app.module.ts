807d472467c743feabd37f848bbd514abbc90c84
Janynne Gomes <janynnegomes@gmail.com>
2020-11-21 18:45:02 -0300

    install ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PurchaseRoutingModule } from './purchase/purchase-routing.module';
 import { PurchaseModule } from './purchase/purchase.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PurchaseModule } from './purchase/purchase.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
